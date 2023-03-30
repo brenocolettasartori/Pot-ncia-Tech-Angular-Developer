6eb4d6c7adc08204a4843681a4acd55fd00c5116
Breno <brenocolettasartori@gmail.com>
2023-03-30 19:14:05 -0300

    adicionado aual de importações e exportações

---
+++ Curso JavaScript/Aula 05 - Importações e Exportaçõess/funcoes-auxiliares.js
@@ -0,0 +1,19 @@
+const entradas = [5, 50, 10, 98, 23]
+let i = 0
+
+function gets() {
+    const valor = entradas[i]
+    i++
+    return valor
+}
+
+function print(texto) {
+    console.log(texto)
+}
+
+
+// para exportar
+module.exports = {
+    gets,
+    print
+}
